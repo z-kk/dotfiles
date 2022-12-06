5739659b6ac570017b06229ad755c27e80f8d37a
z-kk <z-kk@gmail.com>
2022-12-06 19:26:00 +0900

    fix pyenv config

--- .config/bash/profile.sh
+++ .config/bash/profile.sh
@@ -6,7 +6,7 @@ export XDG_CONFIG_HOME=$HOME/.config
 if [ -e ~/.pyenv ]; then
     export PYENV_ROOT=$HOME/.pyenv
     export PATH="$PYENV_ROOT/bin:$PATH"
-    eval "$(pyenv init -)"
+    eval "$(pyenv init --path)"
 fi
 
 # nim
