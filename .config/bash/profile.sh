30187d5f6c8e5a24d7e529e90d86051ff5bb6d4a
z-kk <z-kk@outlook.jp>
2023-02-18 00:19:46 +0900

    support xdg more

--- .config/bash/profile.sh
+++ .config/bash/profile.sh
@@ -4,30 +4,42 @@ fi
 
 export EDITOR=vim
 export LC_COLLATE=C  # lsの並び順
-export XDG_CONFIG_HOME=$HOME/.config
 export OSTYPE
 
-# local bin
-export PATH="$HOME/bin:$PATH"
+# XDG
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_STATE_HOME="$HOME/.local/state"
+
+export HISTFILE="$XDG_STATE_HOME/bash/history"
+export INPUTRC="$XDG_CONFIG_HOME/readline/inputrc"
+export WGETRC="$XDG_CONFIG_HOME/wgetrc"
+export MYSQL_HISTFILE="$XDG_DATA_HOME/mysql_history"
+export SQLITE_HISTORY="$XDG_DATA_HOME/sqlite_history"
+export TASKRC="$XDG_CONFIG_HOME/task/taskrc"
+export TASKDATA="$XDG_DATA_HOME/task"
 
 # pyenv
 if [ -e ~/.pyenv ]; then
-    export PYENV_ROOT=$HOME/.pyenv
+    export PYENV_ROOT="$HOME/.pyenv"
     export PATH="$PYENV_ROOT/bin:$PATH"
     eval "$(pyenv init --path)"
 fi
-
-# nim
-export PATH=$HOME/.nimble/bin:$PATH
 
 # deno
 export DENO_INSTALL="$HOME/.deno"
 export PATH="$DENO_INSTALL/bin:$PATH"
 
+# nim
+export PATH="$HOME/.nimble/bin:$PATH"
+
+# local bin
+export PATH="$HOME/.local/bin:$PATH"
+
 if [[ $OSTYPE =~ linux ]]; then
     # linux
     umask 022
-    export PATH=$PATH:/sbin
     if [[ -n $(grep -i Microsoft /proc/version) ]]; then
         export IS_WSL=1
         if [[ -n $(ps -o command= -p $(ps -o ppid= -p $$) | grep wsltty) ]]; then
---
+++ setup.sh
@@ -0,0 +1,3 @@
+mkdir -p "$XDG_STATE_HOME/bash"
+mkdir -p "$XDG_CACHE_HOME/vim"
+mkdir -p "$XDG_DATA_HOME"
