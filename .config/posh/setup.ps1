02bdc47c8f6104cab4dec8717069b08c5dc45c10
z-kk <z-kk@outlook.jp>
2025-03-06 18:55:43 +0900

    update aliases
    add abbr git rebase
    add abbr opensslenc
    mod posh alias

--- .config/posh/setup.ps1
+++ .config/posh/setup.ps1
@@ -3,3 +3,6 @@ if (-not (Test-Path $profile_dir)) {
     New-Item -ItemType Directory -Path $profile_dir
 }
 echo '. $HOME\.config\posh\profile.ps1' >> $PROFILE
+
+Install-PSResource posh-git
+Install-PSResource Abbr
