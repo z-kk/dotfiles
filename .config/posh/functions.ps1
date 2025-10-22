33e9411609330d83655193fca0f566e11b99934c
z-kk <z-kk@outlook.jp>
2025-10-22 22:18:55 +0900

    yazi

--- .config/posh/functions.ps1
+++ .config/posh/functions.ps1
@@ -1,3 +1,13 @@
 function Set-Theme($name) {
     oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\${name}.omp.json" | Invoke-Expression
 }
+
+function y {
+    $tmp = (New-TemporaryFile).FullName
+    yazi $args --cwd-file="$tmp"
+    $cwd = Get-Content -Path $tmp -Encoding UTF8
+    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+        Set-Location -LiteralPath (Resolve-Path -LiteralPath $cwd).Path
+    }
+    Remove-Item -Path $tmp
+}
