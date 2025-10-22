db95a941550fb9703a7e8b34aaa13384f6f9ad17
z-kk <z-kk@outlook.jp>
2025-10-22 22:18:55 +0900

    yazi

---
+++ .config/yazi/init.lua
@@ -0,0 +1,33 @@
+Status:children_add(function(self)
+	local h = self._current.hovered
+	if h and h.link_to then
+		return " -> " .. tostring(h.link_to)
+	else
+		return ""
+	end
+end, 3300, Status.LEFT)
+
+require("smart-enter"):setup {
+	open_multi = true,
+}
+
+require("toggle-pane"):entry("min-preview")
+
+require("git"):setup()
+
+require("bookmarks"):setup {
+	last_directory = { enable = true, mode = "dir" },
+	persist = "all",
+	desc_format = "full",
+	file_pick_mode = "parent",
+	custom_desc_input = true,
+	show_keys = true,
+	notify = {
+		enable = true,
+		timeout = 2,
+		message = {
+			new = "New bookmark '<key>' -> '<folder>'",
+			delete = "Deleted bookmark in '<key>'",
+		},
+	},
+}
