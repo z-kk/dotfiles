cbdc962a9532945a080fbd3419bb007fb1f23c23
z-kk <z-kk@outlook.jp>
2025-12-17 20:56:13 +0900

    EDITOR env value

--- .config/bash/aliases.bash
+++ .config/bash/aliases.bash
@@ -1,6 +1,6 @@
 alias abbr='abbrev-alias'
 . $XDG_CONFIG_HOME/shell/aliases.sh
 
-alias bashrc='vim $BDOTDIR/bashrc'
-alias bashpr='vim $BDOTDIR/profile.bash'
-alias bashal='vim $BDOTDIR/aliases.bash'
+alias bashrc='$EDITOR $BDOTDIR/bashrc'
+alias bashpr='$EDITOR $BDOTDIR/profile.bash'
+alias bashal='$EDITOR $BDOTDIR/aliases.bash'
--- .config/posh/env.ps1
+++ .config/posh/env.ps1
@@ -5,6 +5,13 @@ Set-Item env:HOME $HOME
 Set-Item env:XDG_CONFIG_HOME (($HOME -replace '\\','/') + "/.config")
 
 #Set-Item env:VIMINIT 'let $MYVIMRC="$XDG_CONFIG_HOME/vim/vimrc" | source $MYVIMRC'
+if (Get-Command nvim.exe -ErrorAction SilentlyContinue) {
+    Set-Item env:EDITOR nvim.exe
+} elseif (Get-Command vim.exe -ErrorAction SilentlyContinue) {
+    Set-Item env:EDITOR vim.exe
+} else {
+    Set-Item env:EDITOR notepad.exe
+}
 
 Set-Item env:_ZO_EXCLUDE_DIRS "$HOME;$env:LOCALAPPDATA/bookmarks/*"
 
