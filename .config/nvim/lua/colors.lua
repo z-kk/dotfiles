866a4a432a4cbf1ba322e0975de48624620d9707
z-kk <z-kk@outlook.jp>
2026-01-08 22:27:32 +0900

    update vim color
    use torte

--- .config/nvim/lua/colors.lua
+++ .config/nvim/lua/colors.lua
@@ -5,6 +5,98 @@ vim.api.nvim_create_autocmd('ColorScheme', {
   callback = function()
     vim.cmd("hi link FernRootText Directory")
     vim.cmd("hi link FernBranchText Directory")
+    vim.cmd("hi link @org.agenda.day Statement")
+    vim.cmd("hi link @org.agenda.tag @org.bold")
+    vim.cmd("hi link @org.agenda.today @org.bold")
+    vim.cmd("hi link @org.agenda.weekend @org.bold")
+    vim.cmd("hi link @org.agenda.header Comment")
+    vim.cmd("hi link @org.agenda.separator Comment")
+
+    if (vim.fn.has('termguicolors') and vim.o.termguicolors or vim.fn.has('gui_running')) then
+      -- helmholtz term color
+      local term_ansi_colors = {
+        '#000000', '#d42c3a', '#1ca800', '#c0a800', '#005dff', '#b148c6', '#00a89a', '#bfbfbf',
+        '#606060', '#ff7676', '#00f200', '#f2f200', '#7d97ff', '#ff70ff', '#00f0f0', '#ffffff',
+      }
+      for i, color in ipairs(term_ansi_colors) do
+        vim.g["terminal_color_" .. (i - 1)] = color
+      end
+    end
+    local title_colors = {
+      { cfg = 68,  cbg = 254, gfg = "#4a86d4", gbg="#d6e6ff" },
+      { cfg = 172, cbg = 254, gfg = "#cc8400", gbg="#ffe0af" },
+      { cfg = 100, cbg = 254, gfg = "#8a8a00", gbg="#fff9c8" },
+      { cfg = 127, cbg = 254, gfg = "#b800b8", gbg="#f6ddf6" },
+      { cfg = 65,  cbg = 254, gfg = "#5f8f3a", gbg="#ddeecc" },
+      { cfg = 97,  cbg = 254, gfg = "#7e64b8", gbg="#ddd3f3" },
+      { cfg = 36,  cbg = 254, gfg = "#159b82", gbg="#cfede7" },
+      { cfg = 140, cbg = 254, gfg = "#9a7fd6", gbg="#e2d8ff" },
+    }
+
+    if vim.o.background == "dark" then
+      title_colors = {
+        { cfg = 75,  cbg = 236, gfg = "#5fa5ff", gbg = "#1f2a3d" },
+        { cfg = 214, cbg = 236, gfg = "#ffa500", gbg = "#332400" },
+        { cfg = 227, cbg = 236, gfg = "#e6e660", gbg = "#3a3a14" },
+        { cfg = 201, cbg = 236, gfg = "#ff00ff", gbg = "#2a1c2a" },
+        { cfg = 119, cbg = 236, gfg = "#9fcb71", gbg = "#202a18" },
+        { cfg = 141, cbg = 236, gfg = "#b89af0", gbg = "#241d33" },
+        { cfg = 41,  cbg = 236, gfg = "#19bc9c", gbg = "#0e2a25" },
+        { cfg = 104, cbg = 236, gfg = "#9b7dd4", gbg = "#201a2e" },
+      }
+
+      vim.cmd("hi CursorLine ctermbg=17 guibg=#00005f")
+      vim.cmd("hi Folded ctermbg=NONE guibg=NONE")
+
+      vim.cmd("hi @org.leading_stars ctermfg=0 guifg=#000000")
+      vim.cmd("hi @org.agenda.scheduled ctermfg=10 guifg=#22f232")
+      vim.cmd("hi @org.keyword.face.HOLD ctermfg=222 guifg=#ffca33")
+      vim.cmd("hi @org.keyword.face.WAIT ctermfg=blue guifg=#2f8fff")
+      vim.cmd("hi @org.keyword.face.IDEA ctermfg=cyan guifg=cyan")
+      vim.cmd("hi @org.keyword.done cterm=bold ctermfg=2 gui=bold guifg=#1f9f30")
+    end
+
+    for i = 1, 8 do
+      local tc = title_colors[i]
+      vim.api.nvim_set_hl(0, "@markup.heading." .. i, { fg = tc["gfg"], ctermfg = tc["cfg"] })
+      vim.api.nvim_set_hl(0, "@org.headline.level" .. i, { fg = tc["gfg"], ctermfg = tc["cfg"] })
+      vim.api.nvim_set_hl(0, "RenderMarkdownH" .. i, { fg = tc["gfg"], ctermfg = tc["cfg"] })
+      vim.api.nvim_set_hl(0, "RenderMarkdownH" .. i .. "Bg", { bg = tc["gbg"], ctermbg = tc["cbg"] })
+    end
+  end
+})
+
+vim.api.nvim_create_autocmd('ColorScheme', {
+  group = colorCmdGrp,
+  pattern = "torte",
+  callback = function()
+    if vim.g.usemycolor ~= 1 then
+      return
+    end
+    vim.cmd("hi StatusLine ctermfg=233 ctermbg=45 guifg=#0e1416 guibg=#00ccff")
+    vim.cmd("hi StatusLineNC ctermfg=16 ctermbg=243 guifg=#000000 guibg=#767676")
+    vim.cmd("hi VertSplit ctermfg=243 ctermbg=NONE guifg=#767676 guibg=NONE")
+    vim.cmd("hi FloatBorder ctermfg=129 guifg=#af00ff")
+
+    vim.cmd("hi Visual ctermfg=NONE ctermbg=129 guifg=NONE guibg=#9529F4")
+    vim.cmd("hi WarningMsg ctermfg=226 guifg=#ffff00")
+    vim.cmd("hi Constant ctermfg=154 guifg=#afff00")
+    vim.cmd("hi String ctermfg=41 guifg=#00d75f")
+    vim.cmd("hi Function ctermfg=147 guifg=#afafff")
+
+    vim.cmd("hi DiffAdd ctermfg=NONE ctermbg=235 guifg=NONE guibg=#262626")
+    vim.cmd("hi DiffChange ctermfg=NONE ctermbg=18 guifg=NONE guibg=#000087")
+    vim.cmd("hi DiffText ctermfg=NONE ctermbg=90 guifg=NONE guibg=#870087")
+    vim.cmd("hi DiffDelete cterm=reverse ctermfg=234 ctermbg=133 gui=reverse guifg=#1c1c1c guibg=#af5faf")
+
+    vim.cmd("hi NeogitDiffAdd guifg=#5faf5f guibg=#003f20")
+    vim.cmd("hi NeogitDiffAddHighlight guifg=#d7ff5f guibg=#1c5f20")
+    vim.cmd("hi NeogitDiffAddCursor guifg=#d7ff5f guibg=#050505")
+    vim.cmd("hi NeogitDiffDelete guifg=#d1d1d1 guibg=#5f0000")
+    vim.cmd("hi NeogitDiffDeleteHighlight guifg=#ffffff guibg=#871c1c")
+    vim.cmd("hi NeogitDiffDeleteCursor guifg=#ffffff guibg=#050505")
+    vim.cmd("hi NeogitHunkHeaderHighlight guifg=#000000 guibg=#d78700")
+    vim.cmd("hi NeogitHunkHeaderCursor guifg=#000000 guibg=#d78700")
   end
 })
 
@@ -12,7 +104,9 @@ vim.api.nvim_create_autocmd('ColorScheme', {
   group = colorCmdGrp,
   pattern = "radicalgoodspeed",
   callback = function()
-    vim.cmd("hi CursorLine ctermbg=17 guibg=#000066")
+    if vim.g.usemycolor ~= 1 then
+      return
+    end
     vim.cmd("hi StatusLine ctermfg=233 ctermbg=45 guifg=#0e1416 guibg=#00ccff")
 
     vim.cmd("hi Pmenu ctermfg=15 ctermbg=60 guibg=#666666 guifg=#eeeeec")
@@ -32,19 +126,6 @@ vim.api.nvim_create_autocmd('ColorScheme', {
 
     vim.cmd("hi htmlTagName ctermfg=226 guifg=#fff847")
     vim.cmd("hi htmlArg ctermfg=213 guifg=#ff88ee")
-
-    vim.cmd("hi @org.leading_stars ctermfg=0 guifg=#000000")
-    vim.cmd("hi @org.agenda.scheduled ctermfg=10 guifg=#22f232")
-    vim.cmd("hi @org.keyword.face.HOLD ctermfg=222 guifg=#ffca33")
-    vim.cmd("hi @org.keyword.face.WAIT ctermfg=blue guifg=#2f8fff")
-    vim.cmd("hi @org.keyword.face.IDEA ctermfg=cyan guifg=cyan")
-    vim.cmd("hi @org.keyword.done cterm=bold ctermfg=2 gui=bold guifg=#1f9f30")
-    vim.cmd("hi link @org.agenda.day Statement")
-    vim.cmd("hi link @org.agenda.tag @org.bold")
-    vim.cmd("hi link @org.agenda.today @org.bold")
-    vim.cmd("hi link @org.agenda.weekend @org.bold")
-    vim.cmd("hi link @org.agenda.header Comment")
-    vim.cmd("hi link @org.agenda.separator Comment")
   end
 })
 
@@ -52,6 +133,9 @@ vim.api.nvim_create_autocmd('ColorScheme', {
   group = colorCmdGrp,
   pattern = "hybrid",
   callback = function()
+    if vim.g.usemycolor ~= 1 then
+      return
+    end
     if vim.o.background == "dark" then
       vim.cmd("hi Normal ctermfg=250")
       vim.cmd("hi Comment ctermfg=78 guifg=#42935f")
@@ -67,7 +151,6 @@ vim.api.nvim_create_autocmd('ColorScheme', {
       vim.cmd("hi DiffDelete ctermbg=236 guibg=#101010")
       vim.cmd("hi DiffText cterm=bold ctermfg=NONE ctermbg=52 guifg=NONE guibg=#551800")
 
-      vim.cmd("hi CursorLine ctermbg=17")
       vim.cmd("hi VertSplit ctermfg=152 ctermbg=236")
       vim.cmd("hi LineNr ctermfg=244")
       vim.cmd("hi Pmenu ctermfg=250 ctermbg=234")
@@ -95,6 +178,7 @@ if vim.fn.has('vcon') == 1 then
   vim.opt.termguicolors = true
 end
 
+vim.g.usemycolor = 1
 function color_exists(name)
   if name == nil then
     return false
@@ -103,12 +187,10 @@ function color_exists(name)
   end
 end
 if color_exists(vim.env.VIMCOLOR) then
-  vim.cmd("colorscheme $VIMCOLOR")
-elseif vim.fn.has("gui_running") == 1 and color_exists("hybrid") then
+  vim.cmd.colorscheme("$VIMCOLOR")
+elseif vim.fn.has("gui_running") == 1 then
   vim.opt.background = "light"
-  vim.cmd("colorscheme hybrid")
-elseif color_exists("radicalgoodspeed") then
-  vim.cmd("colorscheme radicalgoodspeed")
+  vim.cmd.colorscheme("wildcharm")
 else
-  vim.cmd("colorscheme ron")
+  vim.cmd.colorscheme("torte")
 end
--- .config/vim/config/color.vim
+++ .config/vim/config/color.vim
@@ -1,77 +1,129 @@
 augroup ColorCmd
   autocmd!
-  autocmd ColorScheme * {
-    hi link FernRootText     Directory
-    hi link FernBranchText   Directory
-  }
-  autocmd ColorScheme hybrid {
-    if &background == 'dark'
-      hi Normal ctermfg=250
-      hi Comment ctermfg=78 guifg=#42935f
-      hi Constant ctermfg=201
-      hi String ctermfg=208
-      hi Statement ctermfg=40
-      hi PreProc ctermfg=110
-      hi Special ctermfg=181
-      hi Todo cterm=reverse gui=reverse
-
-      hi DiffAdd ctermfg=NONE ctermbg=18 guifg=NONE guibg=#002266
-      hi DiffChange ctermfg=NONE ctermbg=54 guifg=NONE guibg=#332266
-      hi DiffDelete ctermbg=236 guibg=#101010
-      hi DiffText cterm=bold ctermfg=NONE ctermbg=52 guifg=NONE guibg=#551800
-
-      hi CursorLine ctermbg=17
-      hi VertSplit ctermfg=152 ctermbg=236
-      hi LineNr ctermfg=244
-      hi Pmenu ctermfg=250 ctermbg=234
-      hi PmenuSel ctermfg=250
-      hi clear SpecialKey
-      hi link SpecialKey Special
-      hi StatusLine ctermfg=51 ctermbg=21 cterm=NONE
-      hi StatusLineNC ctermfg=239
-    elseif &background == 'light'
-      hi Todo cterm=reverse gui=reverse
-
-      hi DiffAdd guifg=NONE
-      hi DiffChange guifg=NONE
-      hi DiffDelete guifg=NONE
-      hi DiffText guifg=NONE guibg=#c0c0ee
-
-      hi Folded guibg=NONE
-    endif
-  }
-  autocmd ColorScheme radicalgoodspeed {
-    hi CursorLine ctermbg=17 guibg=#000066
-    hi StatusLine ctermfg=233 ctermbg=45 guifg=#0e1416 guibg=#00ccff
-
-    hi Pmenu ctermfg=15 ctermbg=60 guibg=#666666 guifg=#eeeeec
-    hi PmenuSel ctermfg=0 ctermbg=214 guibg=#fcaf3e guifg=#000000
-
-    hi DiffAdd ctermfg=NONE guifg=NONE
-    hi DiffChange ctermfg=NONE ctermbg=18 guifg=NONE guibg=#204a87
-    hi DiffText ctermfg=NONE guifg=NONE
-
-    hi Folded ctermfg=244 guifg=#ae5ff2 guibg=NONE
-    hi Todo ctermbg=237 guibg=#696969
-    hi Directory ctermfg=75 guifg=#729fcf
-    hi Type ctermfg=135 guifg=#ae5ff2
-    hi Keyword ctermfg=226 guifg=#fff847
-    hi Special ctermfg=111 guifg=#7fa7f8
-    hi Error ctermfg=220 guifg=#f9d749
-
-    hi htmlTagName ctermfg=226 guifg=#fff847
-    hi htmlArg ctermfg=213 guifg=#ff88ee
-  }
+  autocmd ColorScheme * call s:allcolorcmd()
+  autocmd ColorScheme torte call s:tortecmd()
+  autocmd ColorScheme hybrid call s:hybridcmd()
+  autocmd ColorScheme radicalgoodspeed call s:radicalgoodspeedcmd()
   if (&t_Co ?? 0) < 88
-    autocmd ColorSchemePre * {
-      hi Normal ctermfg=gray
-      hi Comment ctermfg=darkgreen
-
-      hi CursorLine ctermbg=darkblue
-      hi CursorLineNr ctermbg=darkblue
-    }
+    autocmd ColorSchemePre * call s:legacycolorcmd()
   endif
 augroup END
+
+function s:allcolorcmd()
+  hi link FernRootText     Directory
+  hi link FernBranchText   Directory
+
+  if (has('termguicolors') && &termguicolors) || has('gui_running')
+    let g:terminal_ansi_colors = ['#000000', '#d42c3a', '#1ca800', '#c0a800', '#005dff', '#b148c6', '#00a89a', '#bfbfbf', '#606060', '#ff7676', '#00f200', '#f2f200', '#7d97ff', '#ff70ff', '#00f0f0', '#ffffff']
+    for i in range(g:terminal_ansi_colors->len())
+      let g:terminal_color_{i} = g:terminal_ansi_colors[i]
+    endfor
+  endif
+
+  if &background == 'dark'
+    hi CursorLine ctermbg=17 guibg=#00005f
+    hi Folded ctermbg=NONE guibg=NONE
+  endif
+endfunction
+
+function s:tortecmd()
+  if !g:usemycolor
+    return
+  endif
+
+  hi StatusLine ctermfg=233 ctermbg=45 guifg=#0e1416 guibg=#00ccff
+  hi StatusLineNC ctermfg=16 ctermbg=243 guifg=#000000 guibg=#767676
+  hi VertSplit ctermfg=243 ctermbg=NONE guifg=#767676 guibg=NONE
+  hi FloatBorder ctermfg=129 guifg=#af00ff
+
+  hi Visual ctermfg=0 ctermbg=81 guifg=#000000 guibg=#5fd7ff
+  hi Constant ctermfg=154 guifg=#afff00
+  hi String ctermfg=41 guifg=#00d75f
+  hi Function ctermfg=147 guifg=#afafff
+
+  hi DiffAdd ctermfg=NONE ctermbg=235 guifg=NONE guibg=#262626
+  hi DiffChange ctermfg=NONE ctermbg=18 guifg=NONE guibg=#000087
+  hi DiffText ctermfg=NONE ctermbg=90 guifg=NONE guibg=#870087
+  hi DiffDelete cterm=reverse ctermfg=234 ctermbg=133 gui=reverse guifg=#1c1c1c guibg=#af5faf
+endfunction
+
+function s:hybridcmd()
+  if !g:usemycolor
+    return
+  endif
+
+  if &background == 'dark'
+    hi Normal ctermfg=250
+    hi Comment ctermfg=78 guifg=#42935f
+    hi Constant ctermfg=201
+    hi String ctermfg=208
+    hi Statement ctermfg=40
+    hi PreProc ctermfg=110
+    hi Special ctermfg=181
+    hi Todo cterm=reverse gui=reverse
+
+    hi DiffAdd ctermfg=NONE ctermbg=18 guifg=NONE guibg=#002266
+    hi DiffChange ctermfg=NONE ctermbg=54 guifg=NONE guibg=#332266
+    hi DiffDelete ctermbg=236 guibg=#101010
+    hi DiffText cterm=bold ctermfg=NONE ctermbg=52 guifg=NONE guibg=#551800
+
+    hi VertSplit ctermfg=152 ctermbg=236
+    hi LineNr ctermfg=244
+    hi Pmenu ctermfg=250 ctermbg=234
+    hi PmenuSel ctermfg=250
+    hi clear SpecialKey
+    hi link SpecialKey Special
+    hi StatusLine ctermfg=51 ctermbg=21 cterm=NONE
+    hi StatusLineNC ctermfg=239
+  elseif &background == 'light'
+    hi Todo cterm=reverse gui=reverse
+
+    hi DiffAdd guifg=NONE
+    hi DiffChange guifg=NONE
+    hi DiffDelete guifg=NONE
+    hi DiffText guifg=NONE guibg=#c0c0ee
+
+    hi Folded guibg=NONE
+  endif
+endfunction
+
+function s:radicalgoodspeedcmd()
+  if !g:usemycolor
+    return
+  endif
+
+  hi StatusLine ctermfg=233 ctermbg=45 guifg=#0e1416 guibg=#00ccff
+
+  hi Pmenu ctermfg=15 ctermbg=60 guibg=#666666 guifg=#eeeeec
+  hi PmenuSel ctermfg=0 ctermbg=214 guibg=#fcaf3e guifg=#000000
+
+  hi DiffAdd ctermfg=NONE guifg=NONE
+  hi DiffChange ctermfg=NONE ctermbg=18 guifg=NONE guibg=#204a87
+  hi DiffText ctermfg=NONE guifg=NONE
+
+  hi Folded ctermfg=244 guifg=#ae5ff2 guibg=NONE
+  hi Todo ctermbg=237 guibg=#696969
+  hi Directory ctermfg=75 guifg=#729fcf
+  hi Type ctermfg=135 guifg=#ae5ff2
+  hi Keyword ctermfg=226 guifg=#fff847
+  hi Special ctermfg=111 guifg=#7fa7f8
+  hi Error ctermfg=220 guifg=#f9d749
+
+  hi htmlTagName ctermfg=226 guifg=#fff847
+  hi htmlArg ctermfg=213 guifg=#ff88ee
+endfunction
+
+function s:legacycolorcmd()
+  if !g:usemycolor
+    return
+  endif
+
+  hi Normal ctermfg=gray
+  hi Comment ctermfg=darkgreen
+
+  hi CursorLine ctermbg=darkblue
+  hi CursorLineNr ctermbg=darkblue
+endfunction
 
 set background=dark
 syntax on
@@ -80,16 +132,15 @@ if has('vcon')
   set termguicolors
 endif
 
+let g:usemycolor = 1
 let colorlist = map(split(globpath(&runtimepath, 'colors/*.vim'), '\n'), 'fnamemodify(v:val, ":t:r")')
 if index(colorlist, $VIMCOLOR) > -1
   colorscheme $VIMCOLOR
-elseif has('gui_running') && index(colorlist, 'hybrid') > -1
+elseif has('gui_running')
   set background=light
-  colorscheme hybrid
-elseif (&t_Co ?? 0) >= 256 && index(colorlist, 'radicalgoodspeed') > -1
-  colorscheme radicalgoodspeed
+  colorscheme wildcharm
 else
-  colorscheme ron
+  colorscheme torte
 endif
 
 command SynName echo synIDattr(synID(line('.'), col('.'), 1), 'name')
